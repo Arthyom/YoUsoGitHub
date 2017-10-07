2d77cf1f884bdb2e9c7c036fdf1766e5f393119f
arthyom <allpipiasaaa@hotmail.com>
2017-10-07 00:00:13 -0500

    cambios solo en master

--- puthon.py
+++
@@ -1,4 +0,0 @@
-saludo = "ola k ase"
-print("ola k ase")
-for letra in saludo:
-	print(letra)
---
+++ recuperacion.rb
@@ -0,0 +1,5 @@
+puts 'yo sere modificado, eliminado y recuperado'
+
+puts 'agregando cambios'
+
+puts 'modificando archivos en una nueva rama'
